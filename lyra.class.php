8dfbea00b025041117c585d1a4d66c3296ba63bc
Ildar Karymov <rage-dj@yandex.ru>
2013-07-02 01:55:55 +0700

    Create lyra.class.php

---
+++ lyra.class.php
@@ -0,0 +1,78 @@
+<?php
+
+  /**
+	* @author Ildar Karymov
+	* @version 2.0 alpha
+	*/
+
+	class Lyra {
+
+		private $m_tokens;
+		private $m_code;
+		private $t_classpaths;
+		private $t_vars;
+		private $t_functions;
+		private $t_classes;
+		private $t_constants;
+
+		public function __construct()
+		{
+			// Pre-defining:
+			$this->m_tokens     = array();
+			$this->t_classpaths = array("std");
+			$this->t_vars       = array();
+			$this->t_functions  = array(
+				"sleep" => function(seconds) {
+					if (is_int($seconds)) sleep($seconds);
+				}
+			);
+			$this->t_classes    = array();
+			$this->t_constants  = array(
+				"LYRA_VERSION" => "2.5", 
+				"LYRA_STATUS" => "alpha",
+				"LYRA_e14516d0f98fc0ea189fcb247ffb6cf8" => "PHP R.I.P."
+			);
+		}
+
+		private function addImport($a_path) {
+			$path = str_replace(".", "/", $a_path);
+			$filename_lyra = $path . ".lyra";
+			$filename_php  = $path . ".php";
+
+			if (file_exists($_SERVER['DOCUMENT_ROOT'] . $filename_lyra)) {
+				require $filename_lyra;
+				array_push($this->t_classpaths, $path);
+				return true;
+			}
+			elseif (file_exists($_SERVER['DOCUMENT_ROOT'] . $filename_php)) {
+				require $filename_php;
+				array_push($this->t_classpaths, $path);
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+
+		public function Start($a_code)
+		{
+			$this->m_code = explode("\n", $a_code);
+
+			for ($i = 0; $i < count($this->m_code); $i++) {
+				// Parsing imports:
+				if (preg_match("/(\s|\t)*import(\s)((\w)+\.)+(\w)+;(\s|\t)*/", $this->m_code[$i])) {
+					// @TODO: Implemention with a single regexp
+					$classpath = preg_replace("/(\s|\t)*import(\s)/", "", $this->m_code[$i]);
+					$classpath = preg_replace("/;(\s|\t)*", "", $classpath);
+					$import = $this->addImport($classpath);
+					if (!$import) {
+						// @TODO: Calling degugger
+						# Debugger atata
+					}
+
+					# To be continued...
+				}
+			}
+		}
+
+	}
